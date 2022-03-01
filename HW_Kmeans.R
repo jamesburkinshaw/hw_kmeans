3bd73d54ec1e780de3b59e44243e2fae2af097b4
jamesburkinshaw <99751397+jamesburkinshaw@users.noreply.github.com>
2022-03-01 21:11:10 +0000

    Loads of edits

--- HW_Kmeans.R
+++ HW_Kmeans.R
@@ -1,4 +1,4 @@
-hw_kmeans <- function(x, k, colx, coly, msg=FALSE, doPlot=FALSE, animation=FALSE, interval=0.07, nstart=5, random=FALSE) {
+hw_kmeans <- function(x, k, colx, coly, msg=FALSE, doPlot=FALSE, animation=FALSE, interval=0.07, nstart=5, random=FALSE, save = FALSE) {
   get_centroids <- function(x, k, randomFlag){
     #split data into clusters
     splitx <- split(x, x$cluster)
@@ -55,15 +55,21 @@ hw_kmeans <- function(x, k, colx, coly, msg=FALSE, doPlot=FALSE, animation=FALSE
     return(error)
   }
   
-  referenceError<-Inf
+  #counter for plots
+  plotIdx<-1
   
+  #Initialise reference error to check which start is optimal
+  referenceError<-Inf
   for (s in 1:nstart) {
-    
-    cat('Start ', s, '\n')
+    if (msg) {
+      cat('Start ', s, '\n')
+    }
     
     #Assign points to cluster at random
     cluster <- sample(1:k,nrow(x),replace = TRUE)
     x$cluster <- cluster
+    
+    #x$changes <- 0
     
     #index for sorting
     x$index <- as.numeric(row.names(x))
@@ -71,11 +77,22 @@ hw_kmeans <- function(x, k, colx, coly, msg=FALSE, doPlot=FALSE, animation=FALSE
     #set initial centroids as means of clusters
     centroids <- get_centroids(x,k,random)
     
-    if(doPlot){
+    if(doPlot | animation){
       #plot intial clusters and centroids
       plot(x[c(colx,coly)],col=x$cluster,main='Initial Clusters/Centroids',sub=paste('Start',s,sep=' '))
       points(centroids[c(colx,coly)],col=centroids$cluster,pch=4, cex=3)
     }
+    
+    if(save){
+      #save initial clusters + centroids plot 
+      file <- paste('plots/hw_plot_',plotIdx,'.png',sep='')
+      png(filename=file, width=500,height =500)
+      plot(x[c(colx,coly)],col=x$cluster,main='Initial Clusters/Centroids',sub=paste('Start',s,sep=' '))
+      points(centroids[c(colx,coly)],col=centroids$cluster,pch=4, cex=3)
+      dev.off()
+      plotIdx<-plotIdx+1
+    }
+    
     
     #iterate until
     i <- 1
@@ -121,13 +138,27 @@ hw_kmeans <- function(x, k, colx, coly, msg=FALSE, doPlot=FALSE, animation=FALSE
           }
         }
         
+        #if(x[r,]$cluster != newCluster) {
+          #x[r,]$changes <- x[r,]$changes + 1
+        #}
+        
         x[r,]$cluster <- newCluster
         
-        #plot data with point being tested
         if (animation){
+          #plot data with point being tested
           plot(x[c(colx,coly)],col=x$cluster,main='Clustering in Progress',sub=paste('Iteration', i, 'Point', r, 'Start', s, sep=' '))
           points(p[c(colx,coly)],col=p$cluster,pch=19, cex=1)
-          points(centroids[c(colx,coly)],col=centroids$cluster,pch=4, cex=3) 
+          points(centroids[c(colx,coly)],col=centroids$cluster,pch=4, cex=3)
+        }
+        if (save){
+          #save animation plots
+          file <- paste('plots/hw_plot_',plotIdx,'.png',sep='')
+          png(filename=file, width=500,height =500)
+          plot(x[c(colx,coly)],col=x$cluster,main='Clustering in Progress',sub=paste('Iteration', i, 'Point', r, 'Start', s, sep=' '))
+          points(p[c(colx,coly)],col=p$cluster,pch=19, cex=1)
+          points(centroids[c(colx,coly)],col=centroids$cluster,pch=4, cex=3)
+          dev.off()
+          plotIdx<-plotIdx+1
         }
         #if a cluster has moved loop again
         if (initialCluster != newCluster) {
@@ -143,11 +174,16 @@ hw_kmeans <- function(x, k, colx, coly, msg=FALSE, doPlot=FALSE, animation=FALSE
     
     #calculate sum of squares of clustering start
     currentError <- get_sum_of_squares(x,k,centroids)
-    cat('Error of current cluster ', currentError, '\n')
+    
+    if (msg) {
+      cat('Error of current cluster ', currentError, '\n')
+    }
     
     #if it is the lowest save it to return
     if (currentError < referenceError) {
-      cat('Iteration ', s, ' is the new champion. Current Error=', currentError, '\n')
+      if(msg){
+        cat('Iteration ', s, ' is the new champion. Current Error=', currentError, '\n')
+      }
       startReturn <- s
       clusterReturn <- x
       centroidsReturn <- centroids
@@ -155,19 +191,41 @@ hw_kmeans <- function(x, k, colx, coly, msg=FALSE, doPlot=FALSE, animation=FALSE
     } 
   }
   
-  if(doPlot){
+  if(doPlot | animation){
     #plot final clusters
     plot(clusterReturn[c(colx,coly)],col=clusterReturn$cluster,main='Final Clusters', sub=paste('Start', startReturn, sep=' '))
     points(centroidsReturn[c(colx,coly)],col=centroidsReturn$cluster,pch=4, cex=3)
   }
+  if(save){
+    #save final clusters plot
+    file <- paste('plots/hw_plot_',plotIdx,'.png',sep='')
+    png(filename=file, width=500,height =500)
+    plot(clusterReturn[c(colx,coly)],col=clusterReturn$cluster,main='Final Clusters', sub=paste('Start', startReturn, sep=' '))
+    points(centroidsReturn[c(colx,coly)],col=centroidsReturn$cluster,pch=4, cex=3)
+    dev.off()
+    plotIdx<-plotIdx+1
+  }
+  
+  #normalise centroids by ordering on column
+  centroidsReturn <- centroidsReturn[order(centroidsReturn[,colx]), ]
+  centroidsReturn <- cbind(newCluster = 1:nrow(centroidsReturn), centroidsReturn) 
+  
+  for (r in 1:nrow(clusterReturn)) {
+    for (c in 1:nrow(centroidsReturn))
+      if (clusterReturn[r,]$cluster == centroidsReturn[c,]$cluster){
+        #cat('row: ', r, ' old cluster: ', clusterReturn[r,]$cluster, ' new cluster: ', centroidsReturn[c,]$newCluster, '\n')
+        newRowCluster <- centroidsReturn[c,]$newCluster
+      }
+    clusterReturn[r,]$cluster <- newRowCluster
+  }
+
+  centroidsReturn$cluster <- centroidsReturn$newCluster
+  centroidsReturn <- within(centroidsReturn, rm(newCluster)) 
   
   #reorder data to return
-  #TODO: Fix Class return
-  toReturn <- list(clusterReturn[order(clusterReturn$index), ], centroidsReturn)
-  class(toReturn) <- 'Cluster'
+  toReturn <- list(clusterReturn[order(clusterReturn$index), ], centroidsReturn[order(centroidsReturn$index), ])
   return(toReturn)
 }
-
 
 #iris
 newiris <- iris
@@ -177,10 +235,23 @@ newiris$Species <- NULL
 first="Sepal.Length"
 second="Sepal.Width"
 
+#centroidList<-c()
 #kc <- kmeans(newiris, 3)
-kc <- hw_kmeans(newiris,3, first, second, doPlot = TRUE, random=FALSE)
+#for (i in 1:3) {
+  kc <- hw_kmeans(newiris,3, first, second, nstart = 1)
+  kc
+#  centroidList <- append(centroidList, kc[2])
+#}
+#centroidDf <- data.frame(kc[2])
+#centroidDf$Sepal.Length
+#centroidDf
+#for (row in centroidDf){
+#  print(row[1])
+#}
 
-kc
+
+#centroidList
+
 #table(iris$Species, kc[1]$cluster)
 
 #plot(newiris[c(first, second)], col=kc$cluster)
